5ad7de4f9a80d46b70e167ec7d099a035c265c55
Sasha <97976485+yukka-yu@users.noreply.github.com>
2024-02-22 21:03:14 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my1app',
-    'Authorapp',
+    #'my1app',
+    #'Authorapp',
     'Orderapp',
     #'debug_toolbar',
 ]
