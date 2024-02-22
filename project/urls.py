eecf225c8c716aedeb680f1114ab1bcd4de389dc
Sasha <97976485+yukka-yu@users.noreply.github.com>
2024-02-22 21:34:17 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('a/', include('Authorapp.urls')),
     path('o/', include('Orderapp.urls')),
    # path('__debug__/', include("debug_toolbar.urls")),
 
